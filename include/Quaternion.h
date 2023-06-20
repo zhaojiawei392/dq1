2f12421b643f6944863a16fad51d914c61a1694f
kai <zhaojiawei392@gmail.com>
2023-06-19 19:12:13 -0700

    seperated macro and quaternion

---
+++ include/Quaternion.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Macro.h"
+
+namespace dq1
+{
+
+template<typename Scalar_>
+class Quaternion{
+public:
+    Quaternion(const Evec<Scalar_, 3>& xyz);
+    Quaternion(const Evec<Scalar_, 4>& wxyz);
+    Quaternion(Evec<Scalar_, 3>&& xyz);
+    Quaternion(Evec<Scalar_, 4>&& wxyz);
+
+    // Constructor lvalue
+    Quaternion(const Scalar_& w=Scalar_(), const Scalar_& x=Scalar_(), const Scalar_& y=Scalar_(), const Scalar_& z=Scalar_());
+    // Constructor rvalue
+    Quaternion(Scalar_&& w, Scalar_&& x, Scalar_&& y, Scalar_&& z);
+    Quaternion(Scalar_&& w, Scalar_&& x, Scalar_&& y);
+    Quaternion(Scalar_&& w, Scalar_&& x);
+    Quaternion(Scalar_&& w);
+
+    // Destructor
+    ~Quaternion();
+
+    Scalar_ w() const;
+    Scalar_ x() const;
+    Scalar_ y() const;
+    Scalar_ z() const;
+
+    /**
+     * @brief Calculates the sum of this quaternion and argument quaternion.
+     * This function takes one quaternion and returns the sum of this quaternion and argument quaternion.
+     * @param quaternion The argument quaternion.
+     * @return the sum of this quaternion and argument quaternion.
+     */
+    Quaternion operator+(Quaternion&& quaternion);
+    Quaternion operator-(Quaternion&& quaternion);
+    Quaternion operator*(Quaternion&& quaternion);
+    Quaternion operator*(Scalar_&& scalar);
+    Quaternion operator/(Scalar_&& scalar);
+
+
+
+protected:
+    Scalar_ w_, x_, y_, z_;
+
+};
+
+
+}
