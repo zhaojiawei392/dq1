546a85e71020377fe1ac6a28e82c71c77331153f
kai <zhaojiawei392@gmail.com>
2024-07-27 23:36:00 +0900

    developing

---
+++ build/CMakeFiles/test1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kai/Projects/DualQuaternion/src/Quaternion.cpp" "CMakeFiles/test1.dir/src/Quaternion.cpp.o" "gcc" "CMakeFiles/test1.dir/src/Quaternion.cpp.o.d"
+  "/home/kai/Projects/DualQuaternion/test1.cpp" "CMakeFiles/test1.dir/test1.cpp.o" "gcc" "CMakeFiles/test1.dir/test1.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
