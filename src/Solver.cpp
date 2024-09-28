851356822af73346c74a2be3800a32e7b3f320de
kai <zhaojiawei392@gmail.com>
2024-09-28 18:15:00 +0900

    added solver

---
+++ src/Solver.cpp
@@ -0,0 +1,25 @@
+/** 
+ *     This file is part of dq1.
+ *  
+ *     dq1 is free software: you can redistribute it and/or modify 
+ *     it under the terms of the GNU General Public License as published 
+ *     by the Free Software Foundation, either version 3 of the License, 
+ *     or (at your option) any later version.
+ *  
+ *     dq1 is distributed in the hope that it will be useful, 
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *     See the GNU General Public License for more details.
+ *  
+ *     You should have received a copy of the GNU General Public License
+ *     along with dq1. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *     \file src/Solver.hpp
+ *	   \author Jiawei ZHAO
+ *	   \version 1.0
+ *	   \date 2023-2024
+ */
+
+#include "Solver.hpp"
