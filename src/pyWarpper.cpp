f7a39155290962a0a2483b53b1070ebb1dedbe52
kai <zhaojiawei392@gmail.com>
2024-10-05 22:38:06 +0900

    wrapped quaternion to python

--- src/pyWarpper.cpp
+++ src/pyWarpper.cpp
@@ -23,13 +23,56 @@
  */
 
 #include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
 #include <pybind11/stl.h>
+#include <pybind11/complex.h>
 #include "dq1.hpp"
 
-PYBIND11_MODULE(dq1, m) {
-    pybind11::class_<MyClass>(m, "MyClass")
-        .def(pybind11::init<int>())
-        .def("set_value", &MyClass::set_value)
-        .def("get_value", &MyClass::get_value)
-        .def("print", &MyClass::print);
+namespace py = pybind11;
+
+namespace dq1{
+namespace Template{
+
+
+template<typename qScalar_>
+void bind_quaternion(py::module& m, const std::string& class_name){
+    py::class_<Quaternion<qScalar_>>(m, class_name.c_str())
+        // Constructor
+        .def(py::init<>()) // Default constructor
+        .def(py::init<const Vector4<qScalar_>&>())
+        .def(py::init<qScalar_, qScalar_, qScalar_, qScalar_>())
+        .def(py::init<const Vector3<qScalar_>, qScalar_, qScalar_>())
+        // Expose Members
+        .def("normalize", &Quaternion<qScalar_>::normalize)
+        .def("norm", &Quaternion<qScalar_>::norm)
+        .def("rotation_angle", &Quaternion<qScalar_>::rotation_angle)
+        .def("rotation_axis_vec3", &Quaternion<qScalar_>::rotation_axis_vec3)
+        .def("conj", &Quaternion<qScalar_>::conj)
+        .def("inv", &Quaternion<qScalar_>::inv)
+        .def("to_string", &Quaternion<qScalar_>::to_string)
+        .def("__repr__", [](const Quaternion<qScalar_>& q) {
+            return "<Quaternion " + q.to_string() + ">";
+        })
+        // Operators
+        .def(py::self += py::self)
+        .def(py::self -= py::self)
+        .def(py::self *= py::self)
+        .def(py::self *= qScalar_())
+        .def(py::self /= qScalar_())
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def(-py::self)
+        // Query 
+        .def_property_readonly("w", &Quaternion<qScalar_>::w)
+        .def_property_readonly("x", &Quaternion<qScalar_>::x)
+        .def_property_readonly("y", &Quaternion<qScalar_>::y)
+        .def_property_readonly("z", &Quaternion<qScalar_>::z);
+}
+
+}
+}
+
+PYBIND11_MODULE(dq1, m){
+    dq1::Template::bind_quaternion<float>(m, "Quatf");
+    dq1::Template::bind_quaternion<double>(m, "Quatd");
 }
